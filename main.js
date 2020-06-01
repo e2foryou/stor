f25dd6b558dbf07a6e8efa5201f566b643a3608b
e2foryou <48619172+e2foryou@users.noreply.github.com>
2020-06-01 22:52:55 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const secondDisplay = document.querySelector(".seconds .number");
 
 const countdownContainer = document.querySelector(".countdown-container");
 
-const endDate = new Date("june 04 2020 17:00:00");
+const endDate = new Date("june 15 2020 17:00:00");
 let saleEnded = true;
 
 const updateTimer = () => {
